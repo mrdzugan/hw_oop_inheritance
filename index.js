a57ba324f4b6731dadd2155eee5c2f3f8edca660
Andrei Dzugan <mr.dzugan@gmail.com>
2020-07-23 22:33:01 +0300

    Add Student class & functionality

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 class User {
 	constructor(firstName, lastName) {
 		this._name = firstName;
@@ -8,3 +10,26 @@ class User {
 		return `${this._name} ${this._surname}`;
 	}
 }
+
+class Student extends User {
+	constructor(name, surname, year) {
+		super(name, surname);
+		this._year = year;
+	}
+
+	get year() {
+		return this._year;
+	}
+
+	set year(value) {
+		this._year = value;
+	}
+
+	get course() {
+		return new Date().getFullYear() - this.year;
+	}
+}
+
+const st1 = new Student('Andrei', 'Dzugan', 2017);
+
+console.log(`${st1.fullName} is studying on ${st1.course} course`);
